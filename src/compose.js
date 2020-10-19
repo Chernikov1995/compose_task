d76b6d40dc2e9fb31ff8b0c10c0c795689748ed5
Evgeny Chernikov <chernikovevge@gmail.com>
2020-10-19 18:08:54 +0300

    add sync test

--- src/compose.js
+++ src/compose.js
@@ -1,28 +1,10 @@
-
-
-const compose = (fns) => {
- return  (...args) => {
-   return  fns.reduceRight((result, fn) => {
-    return  fn(...[result, ...args.slice(1)])
-     }, args[0])
-  }
+const compose = (fns) =>{
+	
+ return (...args) => {
+  let last = fns[fns.length-1](...args)
+  return fns.slice(0, fns.length-1).reduceRight((acc, fn) =>{
+  	return fn(acc)
+  	}, last) 
+ }
 }
-// const compose = (fns) => {
-//   return (...args) => {
-//     return fns.reduceRight((acc, fn) => {
-//       return fn[...(...acc)]
-//     }, args)
-//   }
-// }
-
-const f1 = x => x - 8
-const f2 = x => x ** 2
-const f3 = (x, y) => (y > 0 ? x + 3 : x - 3)
-
-const fn = compose([])
-
-console.log(fn('3',1))
-console.log(fn(3,-1))
-
 module.exports = compose
-
--- src/compose.spec.js
+++ src/compose.spec.js
@@ -1,16 +1,40 @@
 const compose = require('./compose.js')
+
 describe('Parallel:', () => {
-  test ('Must be define', () => {
+  test('Must be define', () => {
   	expect(compose).toBeDefined()
   })
-  test ('Simple test', ()=>{
+  test('Base test 1', () => {
   	const f1 = x => x - 8
     const f2 = x => x ** 2
     const f3 = (x, y) => (y > 0 ? x + 3 : x - 3)
 
     const fn = compose([f1, f2, f3])
 
-    expect(fn('3',1)).toBe(1081)
-    expect(fn(3,-1)).toBe(-8)
+    expect(fn('3', 1)).toBe(1081)
+    expect(fn(3, -1)).toBe(-8)
   })
+  test('Base test 2', () => {
+    const f1 = x => x - 8
+    const f2 = x => x ** 2
+    const f3 = (x, y) => (y > 0 ? x + 3 : x - 3)
+
+    const fn = compose([f1, f2, f3])
+
+    expect(fn(3, -1)).toBe(-8)
+  })
+  test('Async test', () => {
+    const f1 = x => x - 8
+    const f2 = x => x ** 2
+    const f3 = (x, y) => (y > 0 ? x + 3 : x - 3)
+
+    const arr = [f1, f2, f3]
+
+    const fn = compose(arr)
+
+    arr.reverse()
+
+    expect(fn('3', 1)).toBe(1081)
+  })
+  
 })
